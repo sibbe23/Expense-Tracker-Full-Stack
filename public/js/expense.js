852b8bf2cb911ed975493c58abb37e0eeab7b1e6
sibbe23 <sibbe23@gmail.com>
2023-02-26 11:44:41 +0530

    Ready for demo

--- public/js/expense.js
+++ public/js/expense.js
@@ -7,32 +7,16 @@ const ul = document.querySelector(".lists");
 //Add expense value to an object
 btn.addEventListener("click", (e) => {
   e.preventDefault();
-  const obj = {
-    amount: amount.value,
-    description: desc.value,
-    category: cat.value,
-  };
+  const obj = { amount: amount.value, description: desc.value,category: cat.value, };
  if(!desc.title){
-    axios
-    .post("http://localhost:3000/expense", obj)
+    axios.post("http://localhost:3000/expense", obj)
     .then((data) => {
       const expense = data.data.result;
-      const li = document.createElement("li");
-      li.className = "li";
-      li.appendChild(
-        document.createTextNode(
-          expense.amount + " " + expense.description + " " + expense.category
-        )
-      );
-      const del = document.createElement("button");
-      const edit = document.createElement("button");
-      del.className = "btn btn-danger del";
-      edit.className = "btn btn-outline-secondary edit";
-      del.appendChild(document.createTextNode("Delete"));
-      edit.appendChild(document.createTextNode("Edit"));
-      li.appendChild(del);
-      li.appendChild(edit);
-      ul.appendChild(li);
+      const li = document.createElement("li");li.className = "li";
+      li.appendChild( document.createTextNode( expense.amount + " " + expense.description + " " + expense.category ));
+      const del = document.createElement("button");const edit = document.createElement("button");
+      del.className = "btn btn-danger del";edit.className = "btn btn-outline-secondary edit";
+      del.appendChild(document.createTextNode("Delete")); edit.appendChild(document.createTextNode("Edit"));li.appendChild(del); li.appendChild(edit); ul.appendChild(li);
       window.location.reload();
     })
     .catch((err) => console.log(err));
@@ -40,53 +24,29 @@ btn.addEventListener("click", (e) => {
     axios.put("http://localhost:3000/"+desc.title,obj)
     .then((data) => {
       const expense = data.data.result;
-        const li = document.createElement("li");
-        li.className = "li";
-        li.appendChild(
-          document.createTextNode(
-            expense.amount + " " +expense.description + " " + expense.category
-          )
-        );
-        const del = document.createElement("button");
-        const edit = document.createElement("button");
-        del.className = "btn btn-danger del";
-        edit.className = "btn btn-outline-secondary  edit";
-        del.appendChild(document.createTextNode("Delete"));
-        edit.appendChild(document.createTextNode("Edit"));
-        li.appendChild(del);
-        li.appendChild(edit);
-        ul.appendChild(li);
-        
+        const li = document.createElement("li");li.className = "li";
+        li.appendChild( document.createTextNode(expense.amount + " " +expense.description + " " + expense.category));
+        const del = document.createElement("button");const edit = document.createElement("button");
+        del.className = "btn btn-danger del";edit.className = "btn btn-outline-secondary  edit";
+        del.appendChild(document.createTextNode("Delete"));edit.appendChild(document.createTextNode("Edit"));
+        li.appendChild(del); li.appendChild(edit); ul.appendChild(li); 
       })
       .catch((err) => console.log(err));
-    
  }
-  
 });
+
 //fetch expense
-axios
-  .get("http://localhost:3000")
+axios.get("http://localhost:3000")
   .then((data) => {
     const expenses = data.data.expenses;
     console.log("");
     for (let i = 0; i < expenses.length; i++) {
-      const li = document.createElement("li");
-      li.className = "li";
+      const li = document.createElement("li");li.className = "li";
       li.setAttribute("id", expenses[i].id);
-      li.appendChild(
-        document.createTextNode(
-          expenses[i].amount + " " + expenses[i].description +" "+expenses[i].category
-        )
-      );
-      const del = document.createElement("button");
-      const edit = document.createElement("button");
-      del.className = "btn btn-danger del";
-      edit.className = "btn btn-outline-secondary edit";
-      del.appendChild(document.createTextNode("Delete"));
-      edit.appendChild(document.createTextNode("Edit"));
-      li.appendChild(del);
-      li.appendChild(edit);
-      ul.appendChild(li);
+      li.appendChild(document.createTextNode(expenses[i].amount + " " + expenses[i].description +" "+expenses[i].category));
+      const del = document.createElement("button"); const edit = document.createElement("button");
+      del.className = "btn btn-danger del"; edit.className = "btn btn-outline-secondary edit";
+      del.appendChild(document.createTextNode("Delete"));edit.appendChild(document.createTextNode("Edit")); li.appendChild(del); li.appendChild(edit); ul.appendChild(li);
     }
   })
   .catch((err) => console.log(err));
@@ -96,12 +56,9 @@ ul.addEventListener("click", (e) => {
   if (e.target.classList.contains("del")) {
     axios
       .delete("http://localhost:3000/" + e.target.parentElement.id)
-      .then((res) => {
-        ul.removeChild(e.target.parentElement);
-      })
+      .then((res) => {ul.removeChild(e.target.parentElement);})
       .catch((err) => console.log(err));
-  }
-});
+  }});
 //edit expense
 ul.addEventListener("click", editEvent);
 function editEvent(e) {
@@ -116,6 +73,4 @@ function editEvent(e) {
         desc.title = expense.id;
         cat.value = expense.category;
       })
-      .catch((err) => console.log(err));
-  }
-}
+      .catch((err) => console.log(err));}}
